8c93d2994724b9bbce36c1b33e87687a686b95a4
Bradley Lund <bradjlund@gmail.com>
2021-09-12 09:28:06 +0200

    Setting up login screen

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,80 +1,14 @@
 import "./App.css";
-import Todo from "./Components/Todo";
-import Form from "./Components/Form";
+import PrivateApp from "./Components/PrivateApp";
+import Login from "./Components/Login";
 import React from "react";
 
+let loggedin = false;
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // initialise the state:
-    this.state = {
-      tasks: [],
-      input: "",
-    };
-
-    // bind the functions to this
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-  }
-
-  // the function for handling the submition of the form
-  handleSubmit(event) {
-    // the event gets passed in from when the button with type submit gets clicked
-    // preventDefault prevents the automatic refreshing of the page and normal behaviour for a submit button
-    event.preventDefault();
-
-    // get the tasks that are currently in the state and use es6 deconstruction to make a new list with the added one
-    let tasksUpdated = [...this.state.tasks, this.state.input];
-
-    console.log(tasksUpdated);
-    // clear the input box once the new task has been added and set the state for tasks to the new array
-    this.setState({ tasks: [...tasksUpdated], input: "" });
-  }
-
-  // the function for handling the state of the input box
-  handleChange(event) {
-    // the onchange method from the input box passes this function the event (the change)
-
-    // inputval gets the value from where the event came from (the input box)
-    let inputVal = event.target.value;
-    // console.log(inputVal);
-
-    // set the new state of the input to the new value with extra letters which gets passed back down in the render method
-    this.setState({ input: inputVal });
-  }
-
-  // handling the deletion of a task
-  handleDelete(name) {
-    // I just took the name of the task, I know this could cause issues if someone wants to do two tasks with the same name but thought it would do for now
-    // I used the filter method to just get the tasks from the state that were not equal to the name of the one we were trying to delete.
-    let updatedTasks = this.state.tasks.filter((task) => task !== name);
-
-    // set the state to the new array without the deleted task
-    this.setState({ tasks: updatedTasks });
-  }
-
+  // here we want to put if logged in then show private app otherwise show the login screen
   render() {
-    return (
-      <div>
-        <h1>Todo List</h1>
-        {/* <Form addTask={addTask} /> */}
-        <Form
-          input={this.state.input}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-        />
-
-        <h2>Tasks to do:</h2>
-        <ul>
-          {/* map through the tasks in the state and use the todo component to give them the necessary html for a list */}
-          {this.state.tasks.map((task, index) => (
-            <Todo name={task} key={index} handleDelete={this.handleDelete} />
-          ))}
-        </ul>
-      </div>
-    );
+    return <div>{loggedin ? <PrivateApp /> : <Login />}</div>;
   }
 }
 
---
+++ react-ui/src/Components/Login.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    //   take the state and post a request to the backend with the details
+    console.log(password, username);
+    axios
+      .post("/login", {
+        username: "Fred",
+        password: "tsitsikama",
+      })
+      .then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
+
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Log in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+
+          <Button
+            id="logInButton"
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleLogin}>
+            Log In
+          </Button>
+          <Button
+            id="registerButton"
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}>
+            Register
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
